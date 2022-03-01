8aa1ec991b0d89ce9a1c3e619137249a90c2a3fa
allenbra17 <allenbra17@gmail.com>
2022-03-01 16:44:30 -0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import stove from "./Assets/stove.png"
+
+function App() {
+  return (
+    <div className="App">
+      <img src={stove} alt="" />
+      <h1 className="title">Whats4Dinner</h1>
+    </div>
+  );
+}
+
+export default App;
